6e632fe6a21acf242c61bae36eb28c91b31ed2c0
Trangtanaa <102002874+Trangtanaa@users.noreply.github.com>
2024-03-16 19:32:07 +0700

    update lab's wifi

--- src/myWifi.h
+++ src/myWifi.h
@@ -6,21 +6,21 @@
 // ssid and password wifi
 #define NUMBER_OF_KNOWN_NETWORKS 10
 const char ssid[NUMBER_OF_KNOWN_NETWORKS][50] = {
-    "do tien hai",
-    "678 Lang-2G",
-    "678 Lang-5G",
-    "EVSELab",
+    // "do tien hai",
+    // "678 Lang-2G",
+    // "678 Lang-5G",
+    // "EVSELab",
     "IPAC LAB 2.4G",
-    "TRANSON1",
-    "Tran Son_5G"};
+    "TRANSON1"
+    };
 const char password[NUMBER_OF_KNOWN_NETWORKS][50] = {
-    "dotienhai",
-    "868686868686",
-    "868686868686",
-    "EVSELab0111200",
-    "biogaslab",
-    "transon999",
-    "transon999"};
+    // "dotienhai",
+    // "868686868686",
+    // "868686868686",
+    // "EVSELab0111200",
+    "12345687",
+    "transon999"
+    };
 #define MAXIMUM_CONNECTING_TIME 5 // 1s per time
 #define CONNECTED 1
 #define FOUND_NO_NETWORK 2
